bb26049b2098af4f2efa98218deaee0489a982ab
L3vi <levistum@gmail.com>
2020-02-13 10:17:38 -0700

    added main view

---
+++ GottaGo/MainView.swift
@@ -0,0 +1,23 @@
+//
+//  MainView.swift
+//  GottaGo
+//
+//  Created by Levi Stum on 2/13/20.
+//  Copyright © 2020 Eric Mathusek. All rights reserved.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        MapView()
+        // Add drawer
+        // Add current location/settings buttons
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
--- GottaGo/MapView.swift
+++ GottaGo/MapView.swift
@@ -16,7 +16,7 @@ struct MapView: UIViewRepresentable {
     
     func updateUIView(_ view: MKMapView, context: Context) {
         let coordinate = CLLocationCoordinate2D(
-            latitude: 34.011286, longitude: -116.166868)
+            latitude: 43.8260227, longitude: -111.7896876)
         let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
         let region = MKCoordinateRegion(center: coordinate, span: span)
         view.setRegion(region, animated: true)
