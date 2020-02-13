bd5a17392f608dd0df7e808f94318e3f569fd63f
L3vi <levistum@gmail.com>
2020-02-13 10:30:31 -0700

    added drawer view

---
+++ GottaGo/DrawerView.swift
@@ -0,0 +1,21 @@
+//
+//  DrawerView.swift
+//  GottaGo
+//
+//  Created by Levi Stum on 2/13/20.
+//  Copyright © 2020 Eric Mathusek. All rights reserved.
+//
+
+import SwiftUI
+
+struct DrawerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DrawerView_Previews: PreviewProvider {
+    static var previews: some View {
+        DrawerView()
+    }
+}
