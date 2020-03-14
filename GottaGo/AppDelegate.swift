6c8aec4aa5e430051e0d172154211c8e4ac00717
L3vi <levistum@gmail.com>
2020-03-13 23:49:18 -0600

    Updated pod stuff

--- GottaGo/AppDelegate.swift
+++ GottaGo/AppDelegate.swift
@@ -12,9 +12,8 @@ import Firebase
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
         // Override point for customization after application launch.
         return true
     }
