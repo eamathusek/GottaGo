6ea5175a2cbdcdcd0065dc71521719162871c308
eamathusek <eamathusek@gmail.com>
2020-04-06 14:14:24 -0500

    Worked on ReviewUI

--- GottaGo/ReviewFormUI.swift
+++ GottaGo/ReviewFormUI.swift
@@ -9,19 +9,63 @@
 import SwiftUI
 
 struct ReviewFormUI: View {
+    @State private var Name = ""
+    @State private var Latitude = ""
+    @State private var Longitude = ""
+    @State private var General = ""
+    @State private var Cleanliness = ""
+    @State private var Location = ""
+    @State private var Accessibility = ""
+    @State private var Traffic = ""
+    
     var body: some View {
         NavigationView{
 
             Form {
-                    Picker(selection: .constant(2), label: Text("Cleanliness")) {
+                Section (header: Text("Bathroom Location")) {
+                    TextField("Name (Optional)", text: $Name)
+                    TextField("Latitude: 43.8231 N", text: $Latitude)
+                    TextField("Longitude: 111.7924° W", text: $Longitude)
+                }
+                
+                Section (header: Text("Ratings")) {
+                    Picker(selection: .constant(1), label: Text("General Rating", tableName: General)) {
                         /*@START_MENU_TOKEN@*/Text("1").tag(1)/*@END_MENU_TOKEN@*/
                         Text("2").tag(2)
                         Text("3").tag(3)
                         Text("4").tag(4)
                         Text("5").tag(5)
+                    }
+                    Picker(selection: .constant(2), label: Text("Cleanliness", tableName: Cleanliness)) {
+                        /*@START_MENU_TOKEN@*/Text("1").tag(1)/*@END_MENU_TOKEN@*/
+                        Text("2").tag(2)
+                        Text("3").tag(3)
+                        Text("4").tag(4)
+                        Text("5").tag(5)
+                    }
+                    Picker(selection: .constant(3), label: Text("Location", tableName: Location)) {
+                        /*@START_MENU_TOKEN@*/Text("1").tag(1)/*@END_MENU_TOKEN@*/
+                        Text("2").tag(2)
+                        Text("3").tag(3)
+                        Text("4").tag(4)
+                        Text("5").tag(5)
+                    }
+                    Picker(selection: .constant(4), label: Text("Accessibility", tableName: Accessibility)) {
+                        /*@START_MENU_TOKEN@*/Text("1").tag(1)/*@END_MENU_TOKEN@*/
+                        Text("2").tag(2)
+                        Text("3").tag(3)
+                        Text("4").tag(4)
+                        Text("5").tag(5)
+                    }
+                    Picker(selection: .constant(1), label: Text("Traffic", tableName: Traffic)) {
+                        /*@START_MENU_TOKEN@*/Text("1").tag(1)/*@END_MENU_TOKEN@*/
+                        Text("2").tag(2)
+                        Text("3").tag(3)
+                        Text("4").tag(4)
+                        Text("5").tag(5)
+                    }
                 }
-                .pickerStyle(WheelPickerStyle())
-            }
+            }.navigationBarTitle(Text("Bathroom Review"))
         }
     }
 }
